efa780a4b862930085231641ab72da477dcc1bca
clb931 <clb931@gmail.com>
2014-10-29 22:59:33 -0500

    Added Text highlighting
    Got text highlighting working for the most part. It now highlights the
    hex values that correspondto a string.

--- html/js/reveng.js
+++ html/js/reveng.js
@@ -1,4 +1,4 @@
-function pad(num, pad)
+function padnum(num, pad)
 {
   return ((1e16 + num.toString(16).toUpperCase())).slice(-pad);
 }
@@ -11,36 +11,41 @@ function handleFileSelect(evt)
 
   reader.onload = function()
   {
+    $('#fileName').text(file.name);
+
     var dataView = new DataView(reader.result);
-    var hex = [];
-    var ascii = [];
+    var hex = "";
+    var ascii = "";
     var lineNum = [];
     var line = 0;
 
     for(var counter = 0; counter < dataView.byteLength; ++counter)
     {
-      hex.push(pad(dataView.getUint8(counter), 2));
-      ascii.push(String.fromCharCode(dataView.getUint8(counter)));
+      hex += padnum(dataView.getUint8(counter), 2) + ' ';
 
       if (dataView.getUint8(counter) === 0x20)
       {
-        ascii[counter] = '_';
+        ascii += ' ';
       }
       else if (dataView.getUint8(counter) < 0x21 || dataView.getUint8(counter) > 0x7E)
       {
-        ascii[counter] = String.fromCharCode(0xFFFF);
+        ascii += '.';
+      }
+      else
+      {
+        ascii += String.fromCharCode(dataView.getUint8(counter));
       }
 
       if (counter % 16 === 0)
       {
-        lineNum.push('0x' + pad(line, 7) + 0);
+        lineNum.push('0x' + padnum(line, 7) + 0);
         line += 1;
       }
-    }
+    };
 
-    $('#hex').text(hex.join(' '));
-    $('#ascii').text(ascii.join(' '));
-    $('.lineNum').text(lineNum.join('\n'));
+    $('#hex').html(hex);
+    $('#ascii').html(ascii);
+    $('.lineNum').html(lineNum.join('\n'));
   }
 
   urlReader.onload = function()
@@ -48,18 +53,25 @@ function handleFileSelect(evt)
     var url = urlReader.result;
     $('body').css('background-image', 'url(' + url + ')');
     document.getElementById('img').src = url;
-  }
 
-  reader.onloadend = function()
-  {
-    $('#fileName').text(file.name);
-  }
+    $('#hex').html('');
+    $('#ascii').html('');
+    $('.lineNum').html('');
+
+    if (file.size > 2000000)
+    {
+      $('#fileName').text('file too big!');
+      alert('file too big!');
+      return;
+    }
+
+    reader.readAsArrayBuffer(file);
+  };
 
   if (file)
   {
     $('#fileName').text('Loading...');
 
-    reader.readAsArrayBuffer(file);
     urlReader.readAsDataURL(file);
   }
 }
@@ -75,9 +87,62 @@ var main = function()
   $('.textArea').scroll(function()
   {
     $('.textArea').not(this).scrollTop($(this).scrollTop());
+    $('.lineNum').scrollTop($(this).scrollTop());
+  });
+
+  //$('#hex').mouseup(function()
+  //{
+  //  var selection = window.getSelection();
+  //  var textArea = $('#ascii');
+
+  //  var diff = 1/3;
+  //  var start = selection.anchorOffset * diff;
+  //  var end = selection.focusOffset * diff;
+  //  var length = end - start;
+
+  //  if (start > end)
+  //  {
+  //    length = start - end;
+  //    start = end;
+  //  }
+
+  //  if (start >= 0)
+  //  {
+  //      textArea.html(textArea.html().substring(0, start)
+  //        + "<span class='highlight'>"
+  //        + textArea.html().substring(start, start + length)
+  //        + "</span>"
+  //        + textArea.html().substring(start + length));
+  //  }
+  //});
+
+  $('#ascii').mouseup(function()
+  {
+    var text = window.getSelection().toString();
+    var textArea = $('#hex');
+
+    var diff = 3;
+    var start = this.innerHTML.indexOf(text) * diff;
+    var length = text.length * diff;
+    if (start >= 0)
+    {
+        textArea.html(textArea.html().substring(0, start)
+          + "<span class='highlight'>"
+          + textArea.html().substring(start, start + length)
+          + "</span>"
+          + textArea.html().substring(start + length));
+    }
+  });
+
+  $('.textArea').mousedown(function()
+  {
+    var wrap = $('.highlight');
+    var text = wrap.text();
+    wrap.replaceWith(text);
+    $(this).removeChild($(this).firstChild);
   });
 
   document.getElementById('fileIn').addEventListener('change', handleFileSelect, false);
-}
+};
 
 $(document).ready(main);
